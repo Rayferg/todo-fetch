349205f912aecd18cee731f9b7bba36cb445f140
Rayferg <Rayferg19@gmail.com>
2022-12-20 16:54:38 +0000

    progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 //include images into your bundle
@@ -7,27 +7,47 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
   const [inputValue, setInputvalue] = useState("");
-  const[todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    getTodo();
+  }, []);
+  const getTodo = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/Rayferg")
+      .then((resp) => resp.json())
+      .then((data) => setTodos(data));
+  };
+  const addTodo = (e) => {
+    e.key === "Enter" &&
+      setTodos(todos.concat({ label: inputValue, done: false }));
+  };
+
   return (
     <div className="container">
       <h1>My Todos</h1>
       <ul>
         <li>
-          <input type="text" 
-		  onChange={(e) => setInputvalue(e.target.value)} 
-		  value={inputValue} onKeyPress= {(e) => e.key === "Enter" && setTodos(todos.concat(inputValue))} 
-		  placeholder="What do you need to do?"></input>
+          <input
+            type="text"
+            onChange={(e) => setInputvalue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => addTodo(e)}
+            placeholder="What do you need to do?"
+          ></input>
         </li>
         {todos.map((item, index) => (
-		<li>
-         {item}{""}<FontAwesomeIcon icon={faTrash} onClick={() => setTodos(
-			todos.filter(
-				(t, currentIndex) => index != currentIndex
-				)
-			)
-		 }></FontAwesomeIcon>
-        </li>
-		))}
+          <li>
+            {item.label}
+            {""}
+            <FontAwesomeIcon
+              icon={faTrash}
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></FontAwesomeIcon>
+          </li>
+        ))}
       </ul>
       <div>{todos.length} tasks</div>
     </div>
